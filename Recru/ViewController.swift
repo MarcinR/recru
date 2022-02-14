7be4b514d80e15fd886bc54f60fbf4cf461a95a1
MarcinR <marcin.ratajczak@appunite.com>
2022-02-14 12:04:05 +0100

    logic implementation'

--- Recru/ViewController.swift
+++ Recru/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var fetchButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func fetchImage() {
+        guard let url = URL(string: textField.text ?? "") else { return }
+        imageView.kf.setImage(with: ImageResource(downloadURL: url, cacheKey: textField.text))
+    }
 
 }
 
