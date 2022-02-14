fa41a822f688dff3f593dd1c010e3c6770cb0b7e
MarcinR <marcin.ratajczak@appunite.com>
2022-02-14 11:10:38 +0100

    initial commit

---
+++ Recru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recru
+//
+//  Created by Marcin Ratajczak on 14/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
